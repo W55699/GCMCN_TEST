b85352962fab1bb01929114731a3e696c5f95ba9
mark zhong <417498134@qq.com>
2024-01-09 23:46:31 +0800

    Update run_ml100k.sh

--- CAGCN-TEST/Paper_setting/run_ml100k.sh
+++ CAGCN-TEST/Paper_setting/run_ml100k.sh
@@ -1,9 +1,9 @@
 echo "=====ml-100k====="
 echo "=====MF====="
-python main.py --dataset='ml-100k' --model='MF'
+python main.py --dataset=''ml-100k'' --model=''MF''
 echo "=====LightGCN====="
-python main.py --dataset='ml-100k' --model='LightGCN'
+python main.py --dataset=''ml-100k'' --model=''LightGCN''
 echo "=====NGCF====="
-python main.py --dataset='ml-100k' --model='NGCF'
+python main.py --dataset=''ml-100k'' --model=''NGCF''
 
 
